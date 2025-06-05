0dd25e5de87e0b01f798999d462c1842eb7572ce
LuzkalidGM <gg2022074263@virtual.upt.pe>
2025-06-04 20:54:13 -0500

    correciones

--- MyMath/Math.Lib/Rooter.cs
+++ MyMath/Math.Lib/Rooter.cs
@@ -15,11 +15,19 @@ namespace Math.Lib
         /// <exception cref="ArgumentOutOfRangeException">
         /// Se lanza cuando el valor de entrada es menor o igual a cero.
         /// </exception>
-        public double SquareRoot(double input)
+        public static double SquareRoot(double input)
         {
-            if (input <= 0.0)
-                throw new ArgumentOutOfRangeException(nameof(input), "El valor ingresado es invalido, solo se puede ingresar números positivos");
-
+            if (input < 0)
+            {
+                throw new System.ArgumentOutOfRangeException("input", "El valor ingresado es invalido, solo se puede ingresar números positivos");
+            }
+            
+            // Permitir 0 ya que la raíz cuadrada de 0 es 0
+            if (input == 0)
+            {
+                return 0;
+            }
+            
             double result = input;
             double previousResult = -input;
             while (System.Math.Abs(previousResult - result) > result / 1000)
